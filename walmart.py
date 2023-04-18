273695ac483c0cb54fe8b8b8491d6e63a5a1e4dc
Ethan Albers <ekalbers22@gmail.com>
2023-04-18 14:32:03 -0400

    get_element function done

--- walmart.py
+++ walmart.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from urllib.parse import urlencode
 from bs4 import BeautifulSoup
 # from playwright.sync_api import sync_playwright
 from threading import Event
@@ -20,13 +22,28 @@ from threading import Event
 #     anti_bot_button = page.get_by_label('Human challenge')
 #     anti_bot_button.wait_for()
 #     anti_bot_button.click(delay=10000)
+proxy_params = {
+    'api_key': 'e9da997b-22c7-4b84-be1b-81e9dfce44d1',
+    'url': "https://www.amazon.com/Nintendo-Switch-Model-Legend-Zelda-Kingdom/dp/B0BZK5M9TD?ref_=Oct_d_onr_d_468642_1&pd_rd_w=IglNg&content-id=amzn1.sym.6ee521d1-d5f5-4884-afbb-3da3aec01d76&pf_rd_p=6ee521d1-d5f5-4884-afbb-3da3aec01d76&pf_rd_r=ZDTBH64VVV03HSZ0EJX1&pd_rd_wg=hai2C&pd_rd_r=6bd80a17-b2dd-4099-b3cf-6f65f0979253&pd_rd_i=B0BZK5M9TD"
+}
 
-url = "https://www.walmart.com/ip/HP-11-6-Chromebook-AMD-A4-4GB-RAM-32GB-Storage-Black-16W64UT-ABA/592161882"
-headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:104.0) Gecko/20100101 Firefox/104.0"}
-html = requests.get(url, headers=headers)
+
+# html = requests.get(url='https://www.sears.com/lg-oled48c2pua-lg-oled48c2p-48-inch-oled-c2pua/p-A109139761')
+html = requests.get(url='https://proxy.scrapeops.io/v1/', params=proxy_params)
 soup = BeautifulSoup(html.text, 'html.parser')
 
-print(soup)
+def find_element(element, attribute, attr_value):
+    counter = 0
+    for child in element.descendants:
+        counter += 1
+        if type(child) == type(element.html):
+            if child.attrs:
+                if attribute in child.attrs.keys():
+                    if attr_value in child.attrs[attribute]:
+                        return child.string
+
+# print(find_element(soup, 'class', 'a-price'))
+print(len(list(soup.descendants)))
 
 
 
